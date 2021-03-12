636673f7cabc58b417fb06657c10c4fe2fefaebe
biasawareprf <79859753+biasawareprf@users.noreply.github.com>
2021-03-12 16:51:01 -0500

    Update Rm3Reranker.java

--- src/main/java/io/anserini/rerank/lib/Rm3Reranker.java
+++ src/main/java/io/anserini/rerank/lib/Rm3Reranker.java
@@ -45,6 +45,9 @@ import java.util.List;
 import java.util.Iterator;
 import java.util.Set;
 
+import java.io.IOException;
+import java.util.*;
+
 import static io.anserini.search.SearchCollection.BREAK_SCORE_TIES_BY_DOCID;
 import static io.anserini.search.SearchCollection.BREAK_SCORE_TIES_BY_TWEETID;
 
@@ -59,7 +62,8 @@ public class Rm3Reranker implements Reranker {
   private final float originalQueryWeight;
   private final boolean outputQuery;
 
-  public Rm3Reranker(Analyzer analyzer, String field, int fbTerms, int fbDocs, float originalQueryWeight, boolean outputQuery) {
+  public Rm3Reranker(Analyzer analyzer, String field, int fbTerms, int fbDocs, float originalQueryWeight,
+      boolean outputQuery) {
     this.analyzer = analyzer;
     this.field = field;
     this.fbTerms = fbTerms;
@@ -70,13 +74,13 @@ public class Rm3Reranker implements Reranker {
 
   @Override
   public ScoredDocuments rerank(ScoredDocuments docs, RerankerContext context) {
-    assert(docs.documents.length == docs.scores.length);
+    assert (docs.documents.length == docs.scores.length);
 
     IndexSearcher searcher = context.getIndexSearcher();
     IndexReader reader = searcher.getIndexReader();
-
-    FeatureVector qfv = FeatureVector.fromTerms(AnalyzerUtils.analyze(analyzer, context.getQueryText())).scaleToUnitL1Norm();
-
+    FeatureVector qfv = FeatureVector.fromTerms(AnalyzerUtils.analyze(analyzer, context.getQueryText()))
+        .scaleToUnitL1Norm();
+    
     boolean useRf = (context.getSearchArgs().rf_qrels != null);
     FeatureVector rm = estimateRelevanceModel(docs, reader, context.getSearchArgs().searchtweets, useRf);
 
@@ -88,11 +92,12 @@ public class Rm3Reranker implements Reranker {
     while (terms.hasNext()) {
       String term = terms.next();
       float prob = rm.getFeatureWeight(term);
-      feedbackQueryBuilder.add(new BoostQuery(new TermQuery(new Term(this.field, term)), prob), BooleanClause.Occur.SHOULD);
+      feedbackQueryBuilder.add(new BoostQuery(new TermQuery(new Term(this.field, term)), prob),
+          BooleanClause.Occur.SHOULD);
     }
 
     Query feedbackQuery = feedbackQueryBuilder.build();
-
+    
     if (this.outputQuery) {
       LOG.info("QID: " + context.getQueryId());
       LOG.info("Original Query: " + context.getQuery().toString(this.field));
@@ -117,7 +122,7 @@ public class Rm3Reranker implements Reranker {
       } else if (context.getSearchArgs().searchtweets) {
         rs = searcher.search(finalQuery, context.getSearchArgs().hits, BREAK_SCORE_TIES_BY_TWEETID, true);
       } else {
-        rs = searcher.search(finalQuery, context.getSearchArgs().hits, BREAK_SCORE_TIES_BY_DOCID, true);
+        rs = searcher.search(finalQuery, 1000, BREAK_SCORE_TIES_BY_DOCID, true);
       }
     } catch (IOException e) {
       e.printStackTrace();
@@ -127,15 +132,15 @@ public class Rm3Reranker implements Reranker {
     return ScoredDocuments.fromTopDocs(rs, searcher);
   }
 
-  private FeatureVector estimateRelevanceModel(ScoredDocuments docs, IndexReader reader, boolean tweetsearch, boolean useRf) {
+  private FeatureVector estimateRelevanceModel(ScoredDocuments docs, IndexReader reader, boolean tweetsearch,
+      boolean useRf) {
     FeatureVector f = new FeatureVector();
 
     Set<String> vocab = new HashSet<>();
     int numdocs;
     if (useRf) {
       numdocs = docs.documents.length;
-    }
-    else {
+    } else {
       numdocs = docs.documents.length < fbDocs ? docs.documents.length : fbDocs;
     }
 
@@ -146,8 +151,7 @@ public class Rm3Reranker implements Reranker {
         continue;
       }
       try {
-        FeatureVector docVector = createdFeatureVector(
-            reader.getTermVector(docs.ids[i], field), reader, tweetsearch);
+        FeatureVector docVector = createdFeatureVector(reader.getTermVector(docs.ids[i], field), reader, tweetsearch);
         docVector.pruneToSize(fbTerms);
         vocab.addAll(docVector.getFeatures());
         docvectors.add(docVector);
@@ -168,8 +172,10 @@ public class Rm3Reranker implements Reranker {
     for (String term : vocab) {
       float fbWeight = 0.0f;
       for (int i = 0; i < docvectors.size(); i++) {
-        // Avoids zero-length feedback documents, which causes division by zero when computing term weights.
-        // Zero-length feedback documents occur (e.g., with CAR17) when a document has only terms 
+        // Avoids zero-length feedback documents, which causes division by zero when
+        // computing term weights.
+        // Zero-length feedback documents occur (e.g., with CAR17) when a document has
+        // only terms
         // that accents (which are indexed, but not selected for feedback).
         if (norms[i] > 0.001f) {
           fbWeight += (docvectors.get(i).getFeatureWeight(term) / norms[i]) * docScores.get(i);
@@ -195,52 +201,75 @@ public class Rm3Reranker implements Reranker {
       while ((text = termsEnum.next()) != null) {
         String term = text.utf8ToString();
 
-        if (term.length() < 2 || term.length() > 20) continue;
-        if (!term.matches("[a-z0-9]+")) continue;
+        if (term.length() < 2 || term.length() > 20)
+          continue;
+        if (!term.matches("[a-z0-9]+"))
+          continue;
 
-        // This seemingly arbitrary logic needs some explanation. See following PR for details:
-        //   https://github.com/castorini/Anserini/pull/289
+        // This seemingly arbitrary logic needs some explanation. See following PR for
+        // details:
+        // https://github.com/castorini/Anserini/pull/289
         //
-        // We have long known that stopwords have a big impact in RM3. If we include stopwords
-        // in feedback, effectiveness is affected negatively. In the previous implementation, we
-        // built custom stopwords lists by selecting top k terms from the collection. We only
-        // had two stopwords lists, for gov2 and for Twitter. The gov2 list is used on all
+        // We have long known that stopwords have a big impact in RM3. If we include
+        // stopwords
+        // in feedback, effectiveness is affected negatively. In the previous
+        // implementation, we
+        // built custom stopwords lists by selecting top k terms from the collection. We
+        // only
+        // had two stopwords lists, for gov2 and for Twitter. The gov2 list is used on
+        // all
         // collections other than Twitter.
         //
-        // The logic below instead uses a df threshold: If a term appears in more than n percent
-        // of the documents, then it is discarded as a feedback term. This heuristic has the
-        // advantage of getting rid of collection-specific stopwords lists, but at the cost of
+        // The logic below instead uses a df threshold: If a term appears in more than n
+        // percent
+        // of the documents, then it is discarded as a feedback term. This heuristic has
+        // the
+        // advantage of getting rid of collection-specific stopwords lists, but at the
+        // cost of
         // introducing an additional tuning parameter.
         //
         // Cognizant of the dangers of (essentially) tuning on test data, here's what I
         // (@lintool) did:
         //
-        // + For newswire collections, I picked a number, 10%, that seemed right. This value
-        //   actually increased effectiveness in most conditions across all newswire collections.
+        // + For newswire collections, I picked a number, 10%, that seemed right. This
+        // value
+        // actually increased effectiveness in most conditions across all newswire
+        // collections.
         //
-        // + This 10% value worked fine on web collections; effectiveness didn't change much.
+        // + This 10% value worked fine on web collections; effectiveness didn't change
+        // much.
         //
-        // Since this was the first and only heuristic value I selected, we're not really tuning
+        // Since this was the first and only heuristic value I selected, we're not
+        // really tuning
         // parameters.
         //
-        // The 10% threshold, however, doesn't work well on tweets because tweets are much
-        // shorter. Based on a list terms in the collection by df: For the Tweets2011 collection,
-        // I found a threshold close to a nice round number that approximated the length of the
-        // current stopwords list, by eyeballing the df values. This turned out to be 1%. I did
-        // this again for the Tweets2013 collection, using the same approach, and obtained a value
+        // The 10% threshold, however, doesn't work well on tweets because tweets are
+        // much
+        // shorter. Based on a list terms in the collection by df: For the Tweets2011
+        // collection,
+        // I found a threshold close to a nice round number that approximated the length
+        // of the
+        // current stopwords list, by eyeballing the df values. This turned out to be
+        // 1%. I did
+        // this again for the Tweets2013 collection, using the same approach, and
+        // obtained a value
         // of 0.7%.
         //
-        // With both values, we obtained effectiveness pretty close to the old values with the
+        // With both values, we obtained effectiveness pretty close to the old values
+        // with the
         // custom stopwords list.
         int df = reader.docFreq(new Term(IndexArgs.CONTENTS, term));
         float ratio = (float) df / numDocs;
         if (tweetsearch) {
           if (numDocs > 100000000) { // Probably Tweets2013
-            if (ratio > 0.007f) continue;
+            if (ratio > 0.007f)
+              continue;
           } else {
-            if (ratio > 0.01f) continue;
+            if (ratio > 0.01f)
+              continue;
           }
-        } else if (ratio > 0.1f) continue;
+        } else if (ratio > 0.1f)
+          continue;
 
         int freq = (int) termsEnum.totalTermFreq();
         f.addFeatureWeight(term, (float) freq);
@@ -253,9 +282,9 @@ public class Rm3Reranker implements Reranker {
 
     return f;
   }
-  
+
   @Override
   public String tag() {
-    return "Rm3(fbDocs="+fbDocs+",fbTerms="+fbTerms+",originalQueryWeight:"+originalQueryWeight+")";
+    return "Rm3(fbDocs=" + fbDocs + ",fbTerms=" + fbTerms + ",originalQueryWeight:" + originalQueryWeight + ")";
   }
 }
